65b390f4b52d10e0a224270d540f05836ef0ff6b
Life is squid <rredzer000@gmail.com>
2024-02-29 23:11:17 +0000

    Builin algorithms: Created function that finds max and min values

--- AlgorithmicTask/Handlers/BuiltinAlgorithsHandler.cs
+++ AlgorithmicTask/Handlers/BuiltinAlgorithsHandler.cs
@@ -1,6 +1,28 @@
-﻿namespace AlgorithmicTask.Handlers
+﻿using AlgorithmicTask.Core;
+
+namespace AlgorithmicTask.Handlers
 {
     public class BuiltinAlgorithsHandler
     {
+        public Result<string> FindMinMax(IList<int> nums, out int min, out int max)
+        {
+            min = 0;
+            max = 0;
+
+            if (nums == null || nums.Count == 0) return Result<string>.Failure("Empty data");
+
+            int numsLength = nums.Count;
+
+            if (numsLength == 1)
+            {
+                min = nums[0];
+                max = nums[0];
+                return Result<string>.Success("");
+            }
+
+            min = nums.Min();
+            max = nums.Max();
+            return Result<string>.Success("");
+        }
     }
 }
