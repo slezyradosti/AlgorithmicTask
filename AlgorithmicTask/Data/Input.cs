659c6f762e024b1a212542964853349e60f1866a
Life is squid <rredzer000@gmail.com>
2024-02-29 22:18:05 +0000

    Created models for input and output

---
+++ AlgorithmicTask/Data/Input.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgorithmicTask.Data
+{
+    public class Input
+    {
+        public string Filename { get; set; }
+        public string FilePath { get; set; }
+        public List<int> Numbers { get; set; } = new List<int>();
+    }
+}
---
+++ AlgorithmicTask/Data/Output.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgorithmicTask.Data
+{
+    public class Outcome
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Median { get; set; }
+        public int Mean { get; set; }
+        public int MaxSequenceLengthASC { get; set; }
+        public string MaxSequenceDataASC { get; set; }
+        public int MaxSequenceLengthDESC { get; set; }
+        public string MaxSequenceDataDESC { get; set; }
+    }
+}
