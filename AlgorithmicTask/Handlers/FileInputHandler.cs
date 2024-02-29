0eeb4b03800ae170cb1aae1127e4c982c42af3f9
Life is squid <rredzer000@gmail.com>
2024-02-29 22:24:48 +0000

    Created class FileHandler Created a function that reads data from file and returns string array with separated numers

---
+++ AlgorithmicTask/Handlers/FileInputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace AlgorithmicTask.Handlers
+{
+    public class FileInputHandler
+    {
+        public async Task<string[]> ReadDataFromFileToStringAsync(string fileName)
+        {
+            string inputDate = "";
+            try
+            {
+                using (var sr = new StreamReader(fileName))
+                {
+                    inputDate = await sr.ReadToEndAsync();
+                }
+
+                return inputDate.Trim(' ', '\n', '\r').Split('\n');
+            }
+            catch (Exception e)
+            {
+                return new string[] { "The file could not be read:\n", e.Message };
+            }
+        }
+    }
+}
