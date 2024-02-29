18b25b1c4703a66494a0f343397d3fca67140026
Life is squid <rredzer000@gmail.com>
2024-02-29 22:40:02 +0000

    Impemented Result type class to File Read function

--- AlgorithmicTask/Handlers/FileInputHandler.cs
+++ AlgorithmicTask/Handlers/FileInputHandler.cs
@@ -1,25 +1,33 @@
-﻿using System.Net;
+﻿using AlgorithmicTask.Core;
+using System.Net;
 
 namespace AlgorithmicTask.Handlers
 {
     public class FileInputHandler
     {
-        public async Task<string[]> ReadDataFromFileToStringAsync(string fileName)
+        public async Task<Result<string[]>> ReadDataFromFileToStringAsync(string fileName)
         {
-            string inputDate = "";
+            string inputData = "";
             try
             {
                 using (var sr = new StreamReader(fileName))
                 {
-                    inputDate = await sr.ReadToEndAsync();
+                    inputData = await sr.ReadToEndAsync();
                 }
 
-                return inputDate.Trim(' ', '\n', '\r').Split('\n');
+                if (string.IsNullOrEmpty(inputData))
+                {
+                    return Result<string[]>.Failure("Empty data.");
+                }
+
+                return Result<string[]>.Success(inputData.Trim(' ', '\n', '\r').Split('\n'));
             }
             catch (Exception e)
             {
-                return new string[] { "The file could not be read:\n", e.Message };
+                return Result<string[]>.Failure("The file could not be read:\n" + e.Message);
             }
         }
+
+        
     }
 }
