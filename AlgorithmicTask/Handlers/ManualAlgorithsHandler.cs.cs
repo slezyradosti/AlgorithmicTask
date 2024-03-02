132b65ae47f4c4214bb22a62be23d1131a13fdde
Life is squid <rredzer000@gmail.com>
2024-03-02 22:24:18 +0000

    Manual Algorithms: Was made a decision to use QuickSort algorithm. The reasons are: 1. Much easier to get max and min values 2. List (or array) must be sorted for finding a median

--- AlgorithmicTask/Handlers/ManualAlgorithsHandler.cs.cs
+++ AlgorithmicTask/Handlers/ManualAlgorithsHandler.cs.cs
@@ -1,6 +1,47 @@
-﻿namespace AlgorithmicTask.Handlers
+﻿using System;
+
+namespace AlgorithmicTask.Handlers
 {
     public class ManualAlgorithsHandler
     {
+        private List<int> _nums;
+
+        private void SortASaveToList(IList<int> nums, int leftIndex, int rightIndex)
+        {
+            _nums = QuickSort(nums, leftIndex, rightIndex).ToList();
+        }
+
+        private IList<int> QuickSort(IList<int> nums, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = nums[leftIndex];
+            while (i <= j)
+            {
+                while (nums[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (nums[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                QuickSort(nums, leftIndex, j);
+            if (i < rightIndex)
+                QuickSort(nums, i, rightIndex);
+            return nums;
+        }
     }
 }
